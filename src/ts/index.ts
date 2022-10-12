4d46912c6a23958e56153ad602968037d8ccf031
Alvaro Parada <alvaro.parada92@gmail.com>
2022-10-12 20:48:11 +0200

    Rename header's title

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,15 @@
 import Component from "./components/Component.js";
 import Header from "./components/Header/Header.js";
+import MainSeries from "./components/MainSeries/MainSeries.js";
 
 const rootElement = document.querySelector(".root");
 
 const container = new Component(rootElement as HTMLElement, "container", "div");
 container.render();
+
 const header = new Header(
   container.domElement,
-  "Series List",
+  "My Series",
   "main-header",
   "header"
 );
