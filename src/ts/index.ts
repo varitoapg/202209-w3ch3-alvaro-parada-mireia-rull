2a6aa2a987c91a541dca5fe6bcbc5763c39e8927
Alvaro Parada <alvaro.parada92@gmail.com>
2022-10-12 21:57:21 +0200

    Add MainSeries to index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
 import Component from "./components/Component.js";
 import Header from "./components/Header/Header.js";
+import MainSeries from "./components/MainSeries/MainSeries.js";
+import SeriesList from "./components/SeriesList/SeriesList.js";
 
 const rootElement = document.querySelector(".root");
 
@@ -8,8 +10,33 @@ container.render();
 
 const header = new Header(
   container.domElement,
-  "Series List",
+  "My Series",
   "main-header",
   "header"
 );
 header.render();
+
+const main = new Component(container.domElement, "main", "main");
+main.render();
+
+const sectionSeries = new MainSeries(
+  main.domElement,
+  "Series List",
+  "series",
+  "section"
+);
+sectionSeries.render();
+
+const seriesPending = new SeriesList(
+  sectionSeries.domElement,
+  "Pending Series",
+  "You have 4 series pending to watch"
+);
+seriesPending.render();
+
+const seriesWatched = new SeriesList(
+  sectionSeries.domElement,
+  "Watched series",
+  "You have watched 4 series"
+);
+seriesWatched.render();
