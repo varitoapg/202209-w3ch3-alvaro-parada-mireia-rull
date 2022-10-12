480f81020376f6aef928e72714684be30a50f0a8
Alvaro Parada <alvaro.parada92@gmail.com>
2022-10-12 19:55:41 +0200

    Add Header and main-title to index

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,14 @@
+import Component from "./components/Component.js";
+import Header from "./components/Header/Header.js";
 
+const rootElement = document.querySelector(".root");
+
+const container = new Component(rootElement as HTMLElement, "container", "div");
+container.render();
+const header = new Header(
+  container.domElement,
+  "Series List",
+  "main-header",
+  "header"
+);
+header.render();
