7ef3c53fd4745a282ddbe6bdda77648bdfdb2c5b
Alvaro Parada <alvaro.parada92@gmail.com>
2022-10-12 21:39:48 +0200

    "Resolver index.ts conflicts, fixed a title typo"

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,42 @@
 import Component from "./components/Component.js";
 import Header from "./components/Header/Header.js";
+// Import MainSeries from "./components/MainSeries/MainSeries.js";
+import SeriesList from "./components/SeriesList/SeriesList.js";
 
 const rootElement = document.querySelector(".root");
 
 const container = new Component(rootElement as HTMLElement, "container", "div");
 container.render();
+
 const header = new Header(
   container.domElement,
-  "Series List",
+  "My Series",
   "main-header",
   "header"
 );
 header.render();
+
+const main = new Component(container.domElement, "main", "main");
+main.render();
+
+const sectionSeries = new MainSeries(
+  main.domElement,
+  "Series List",
+  "series",
+  "section"
+);
+sectionSeries.render();
+
+const seriesPending = new SeriesList(
+  sectionSeries.domElement,
+  "Pending Series",
+  "You have 4 series pending to watch"
+);
+seriesPending.render();
+
+const seriesWatched = new SeriesList(
+  sectionSeries.domElement,
+  "Watched series",
+  "You have watched 4 series"
+);
+seriesWatched.render();
