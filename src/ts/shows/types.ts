166efe7b818af4070b1fbc388d6261be31a26dc7
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-10-13 07:05:58 +0200

    Add show component

---
+++ src/ts/shows/types.ts
@@ -0,0 +1,12 @@
+interface TvShow {
+  id: number;
+  name: string;
+  creator: string;
+  year: number;
+  poster: string;
+  watched: boolean;
+  score: number;
+  emmies: number;
+}
+
+export default TvShow;
